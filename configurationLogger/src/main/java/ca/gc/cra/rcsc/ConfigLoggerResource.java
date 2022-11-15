3f042dca42db27d9fc791c169f05c8ef20a9cdbc
Ashish Kumar <Ashish.Kumar@cra-arc.gc.ca>
2022-11-15 21:46:30 +0000

    Add endpoints to list config maps, secrets and data inside config-map

--- configurationLogger/src/main/java/ca/gc/cra/rcsc/ConfigLoggerResource.java
+++ configurationLogger/src/main/java/ca/gc/cra/rcsc/ConfigLoggerResource.java
@@ -6,27 +6,60 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-
+import java.util.*;
+import javax.ws.rs.*;
+// import org.eclipse.microprofile.config.Config;
+// import org.eclipse.microprofile.config.ConfigProvider;
+// import org.eclipse.microprofile.config.inject.ConfigProperty;
 @Path("/configurations")
 public class ConfigLoggerResource {
+    
 
-    @ConfigProperty(name = "quarkus.resteasy-reactive.path") 
-    String apiBasePath;
+    // Config config = ConfigProvider.getConfig();
+    // Map configValues = config.getValue("config.gitops-microservice-build.data", Map.class);
 
-    @ConfigProperty(name="quarkus.kubernetes-client.master-url")
-    String url;
+    @ConfigProperty(name = "quarkus.kubernetes-client.namespace") 
+    String namespace;
+
+    @ConfigProperty(name = "quarkus.openshift.env.configmaps")
+    String configMapsList;
+
+    @ConfigProperty(name = "quarkus.openshift.env.secrets")
+    String secretsList;
+
+    @ConfigProperty(name = "config.gitops-microservice-build.data")
+    String gitopsMicroserviceBuildData;
+
 
     @GET
     @Path("/config-maps")
     @Produces(MediaType.TEXT_PLAIN)
-    public void getConfigMaps() {
-        System.out.println("API's base path is: "+ apiBasePath);
+    public String getConfigMaps() {
+        System.out.println("List of Config Maps is: "+ configMapsList);
+        return "List of Config Maps is: "+ configMapsList;
+    }
+
+    @GET
+    @Path("/config-maps/{name}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String configMapData(@PathParam("name") String name){
+        String message;
+        switch(name){
+            case "gitops-microservice-build" :
+                message = "Data from "+name+ " is \n"+gitopsMicroserviceBuildData;
+                break;
+            default:
+                message = "Provided config map does not exist";
+        }
+        System.out.println(message);
+        return message;
     }
 
     @GET
     @Path("/secrets")
     @Produces(MediaType.TEXT_PLAIN)
-    public void getSecrets() {
-        System.out.println("OCP url is: "+ url);
+    public String getSecrets() {
+        System.out.println("List of Secrets is: "+ secretsList);
+        return "List of Secrets is: "+ secretsList;
     }
 }
---
+++ configurationLogger/src/main/java/ca/gc/cra/rcsc/GitopsApp.java
@@ -0,0 +1,9 @@
+package ca.gc.cra.rcsc;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api/v1")
+public class GitopsApp extends Application {
+
+}
