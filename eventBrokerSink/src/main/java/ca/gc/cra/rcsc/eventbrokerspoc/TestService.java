f90c8b05e2e9f635eb211a63556f7d17ad50b310
Danial Asghar <46968467+danialasghar@users.noreply.github.com>
2023-01-19 14:20:01 +0000

    fixing some cleanup

--- eventBrokerSink/src/main/java/ca/gc/cra/rcsc/eventbrokerspoc/TestService.java
+++ eventBrokerSink/src/main/java/ca/gc/cra/rcsc/eventbrokerspoc/TestService.java
@@ -7,11 +7,6 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import ca.gc.cra.rcsc.eventbrokerspoc.sender.Storage;
-import ca.gc.cra.rcsc.eventbrokerspoc.sinks.ActiveMQ;
-import ca.gc.cra.rcsc.eventbrokerspoc.sinks.ApacheKafka;
-import ca.gc.cra.rcsc.eventbrokerspoc.sinks.IbmMQ;
-import ca.gc.cra.rcsc.eventbrokerspoc.sinks.NatsBroker;
-import ca.gc.cra.rcsc.eventbrokerspoc.sinks.RabbitMQ;
 import ca.gc.cra.rcsc.eventbrokerspoc.sinks.SolacePubSub;
 
 import java.io.IOException;
@@ -23,16 +18,8 @@ public class TestService {
 	@Inject
 	Storage storage;
 
-	@Inject
-	ActiveMQ activeMq;
-	@Inject
-	ApacheKafka kafka;
 
 	private SolacePubSub solace;
-	private NatsBroker nats;
-	private RabbitMQ rabbitMq;
-	private IbmMQ ibmMq;
-	
 	
 	@GET
     @Path("/solace")
